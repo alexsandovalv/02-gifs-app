0e1de431690bad98413af3ecf06c3a2bf1e8dd97
Luis Alexander Sandoval Vicente <alexander.sandoval@hpe.com>
2023-04-10 15:33:58 -0500

    Fin de la seccion 6 Aplicacion para buscar Imagenes Gifs

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,24 @@
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {GifsService} from "../services/gifs.service";
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent {
+
+  @ViewChild('txtBbuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  constructor( private gifService: GifsService) {
+  }
+  buscar(  ){
+
+    const valor = this.txtBuscar.nativeElement.value;
+    if( valor.trim().length == 0) return;
+
+    this.gifService.buscarGifs(valor);
+    this.txtBuscar.nativeElement.value = '';
+  }
+}
